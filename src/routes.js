5e3a0208509914d289cfa03857fc341956ffbb7b
gabriel silva <gabrielp.siilva@gmail.com>
2019-08-20 21:36:35 -0300

    First commit

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const DevController = require('./controllers/dev.controller');
+const LikeController = require('./controllers/like.controller');
+const DislikeController = require('./controllers/dislike.controller');
+
+const routes = express.Router();
+
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
+
+routes.post('/devs/:devId/likes', LikeController.store);
+routes.post('/devs/:devId/dislikes', DislikeController.store);
+
+module.exports = routes;
